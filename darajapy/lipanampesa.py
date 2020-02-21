5ea1c831956bc8810d93338f41d805757b295971
kamauvick <kamauvick@outlook.com>
2020-02-21 04:29:47 +0300

    Update the query parameters on the lnm-online and c2b functions.

---
+++ darajapy/lipanampesa.py
@@ -0,0 +1,36 @@
+import requests
+from datetime import datetime
+from decouple import config, Csv
+from keys import *
+from access_token import generate_access_token
+from utils import generate_timestamp
+from password import generate_password
+
+# Execute all functions
+formatted_time = generate_timestamp()
+password = generate_password(formatted_time)
+my_access_token = generate_access_token()
+
+
+def lipa_na_mpesa(phonenumber, amount, shortcode, callback_url):
+    print('Start excecuting the Simulate LNMOnline function:::')
+    access_token = my_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = { "Authorization": "Bearer %s" % access_token }
+    request = {
+        "BusinessShortCode": shortcode,
+        "Password": password,
+        "Timestamp": formatted_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        "PartyA": phonenumber,
+        "PartyB": shortcode,
+        "PhoneNumber":phonenumber,
+        "CallBackURL": callback_url,
+        "AccountReference": "vicks_test",
+        "TransactionDesc": "Pay for internet"
+    }
+    
+    response = requests.post(api_url, json = request, headers=headers)
+    return response.text
+    
